fdedbb0617443bb6026a9a191d02257f9356e55e
Thijs van der Heijden <thijsvanderheijden@yoast.com>
2021-11-22 09:26:09 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'vendor/autoload.php';
+$day = $argv[1];
+$challange = $argv[2];
+
+$cname = "Thijsvanderheijden\Adventofcode\Day{$day}\Challenge";
+$d1 = new $cname();
+
+if($challange === 1) {
+	$d1->solveFirst();
+}else{
+	$d1->solveSecond();
+}
---
+++ src/Day17/Challenge.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Thijsvanderheijden\Adventofcode\Day1;
+
+
+use Thijsvanderheijden\Adventofcode\Base\ChallengeBase;
+
+class Challenge extends ChallengeBase {
+
+
+	/**
+	 * Day1Challenge constructor.
+	 */
+	public function __construct() {
+		$this->relativePath = __DIR__;
+		parent::__construct();
+	}
+
+	public function solveFirst(  ) {
+		dump('wowfirst');
+	}
+	public function solveSecond(  ) {
+		dump('wowsecond');
+	}
+}
