1637801c3b3d453c5d0f57ad1193b05f069eb369
Thijs van der Heijden <thijsvanderheijden@yoast.com>
2021-12-03 16:41:05 +0100

    Day 3

--- src/Day3/Challenge.php
+++ src/Day3/Challenge.php
@@ -17,10 +17,44 @@ class Challenge extends ChallengeBase {
 		parent::__construct();
 	}
 
-	public function solveFirst(  ) {
-		dump('wowfirst');
+	public function solveFirst() {
+
+		$gRate = '';
+		$eRate = '';
+		$map   = [];
+
+		for ( $i = 0; $i < 12; $i ++ ) {
+
+			$map[ $i ]   = [];
+			$map[ $i ][] = 0;
+			$map[ $i ][] = 0;
+			foreach ( $this->lines as $totalLine ) {
+				if ( $totalLine[ $i ] == 1 ) {
+					$map[ $i ][1] ++;
+				} else {
+					$map[ $i ][0] ++;
+				}
+			}
+		}
+
+		foreach ( $map as $m ) {
+			if ( $m[0] > $m[1] ) {
+				$gRate .= 0;
+				$eRate .= 1;
+			} else {
+				$gRate .= 1;
+				$eRate .= 0;
+			}
+		}
+		$intg = bindec( $gRate );
+		$inte = bindec( $eRate );
+		dd( $intg * $inte );
 	}
-	public function solveSecond(  ) {
-		dump('wowsecond');
+
+	public function solveSecond() {
+		$solver = new RatingGenerator();
+		$oxyBit = bindec( $solver->getOxyGenBit( $this->lines ) );
+		$co2    = bindec( $solver->getCo2GenBit( $this->lines ) );
+		dd( $oxyBit * $co2 );
 	}
 }
