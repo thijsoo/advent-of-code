ef7af2b1004f621d5bfe86ee5747354ee26267f3
Thijs van der Heijden <thijsvanderheijden@yoast.com>
2021-12-11 11:14:29 +0100

    Day 11

--- src/Day11/Challenge.php
+++ src/Day11/Challenge.php
@@ -1,26 +1,35 @@
 <?php
 
 
-namespace Thijsvanderheijden\Adventofcode\Day1;
+namespace Thijsvanderheijden\Adventofcode\Day11;
 
 
 use Thijsvanderheijden\Adventofcode\Base\ChallengeBase;
 
-class Challenge extends ChallengeBase {
+class Challenge extends ChallengeBase
+{
 
 
-	/**
-	 * Day1Challenge constructor.
-	 */
-	public function __construct() {
-		$this->relativePath = __DIR__;
-		parent::__construct();
-	}
+    /**
+     * Day1Challenge constructor.
+     */
+    public function __construct()
+    {
+        $this->relativePath = __DIR__;
+        parent::__construct();
+    }
 
-	public function solveFirst(  ) {
-		dump('wowfirst');
-	}
-	public function solveSecond(  ) {
-		dump('wowsecond');
-	}
+    public function solveFirst()
+    {
+        $fc = new FlashCounter($this->lines);
+        $c = $fc->stepThroughGrid();
+        dd($c);
+    }
+
+    public function solveSecond()
+    {
+        $fc = new FlashCounter($this->lines);
+        $c = $fc->stepTillSuperFlash();
+        dd($c);
+    }
 }
